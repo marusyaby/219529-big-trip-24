4c343830096ee78e77973bbacfa640da5f7dbf03
marusyaby <mariakhrol@gmail.com>
2024-09-06 23:39:22 +0300

    Add checked state to 1st tripFilterElement

--- src/view/templates/trip-filters-template.js
+++ src/view/templates/trip-filters-template.js
@@ -1,32 +1,34 @@
+const FILTER_VALUES = [
+  'everything',
+  'future',
+  'present',
+  'past'
+];
+
+function createTripFilterTemplate(value, isChecked) {
+  return `
+    <div class="trip-filters__filter">
+                  <input id="filter-${value}" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="${value}" ${isChecked ? 'checked' : ''}>
+                  <label class="trip-filters__filter-label" for="filter-${value}">${value}</label>
+                </div>
+  `;
+}
+
 export function createTripFiltersTemplate() {
+  const tripFilterElements = FILTER_VALUES.map((value, index) =>
+    createTripFilterTemplate(value, index === 0)).join('');
+
   return `
     <div class="trip-main__trip-controls  trip-controls">
-            <div class="trip-controls__filters">
-              <h2 class="visually-hidden">Filter events</h2>
-              <form class="trip-filters" action="#" method="get">
-                <div class="trip-filters__filter">
-                  <input id="filter-everything" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="everything" checked>
-                  <label class="trip-filters__filter-label" for="filter-everything">Everything</label>
-                </div>
+      <div class="trip-controls__filters">
+        <h2 class="visually-hidden">Filter events</h2>
+        <form class="trip-filters" action="#" method="get">
 
-                <div class="trip-filters__filter">
-                  <input id="filter-future" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="future">
-                  <label class="trip-filters__filter-label" for="filter-future">Future</label>
-                </div>
+          ${tripFilterElements}
 
-                <div class="trip-filters__filter">
-                  <input id="filter-present" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="present">
-                  <label class="trip-filters__filter-label" for="filter-present">Present</label>
-                </div>
-
-                <div class="trip-filters__filter">
-                  <input id="filter-past" class="trip-filters__filter-input  visually-hidden" type="radio" name="trip-filter" value="past">
-                  <label class="trip-filters__filter-label" for="filter-past">Past</label>
-                </div>
-
-                <button class="visually-hidden" type="submit">Accept filter</button>
-              </form>
-            </div>
-          </div>
+          <button class="visually-hidden" type="submit">Accept filter</button>
+        </form>
+      </div>
+    </div>
   `;
 }
