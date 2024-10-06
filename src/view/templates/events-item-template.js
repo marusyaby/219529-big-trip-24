01e14fcffc476ee956f10ed35b93159b1d54c5ff
marusyaby <mariakhrol@gmail.com>
2024-10-06 16:34:20 +0300

    Refactor templates

--- src/view/templates/events-item-template.js
+++ src/view/templates/events-item-template.js
@@ -20,23 +20,29 @@ const createOffersTemplate = (selectedOffers) =>
 
 export const createEventsItemTemplate = (event, destination, selectedOffers) => {
   const {basePrice, isFavorite, type, dateFrom, dateTo} = event;
-  const {name} = destination;
+  const city = capitalizeFirstLetter(destination.name);
+  const capitalizedType = capitalizeFirstLetter(type);
+  const startDate = formatDate(dateFrom, Format.DATE);
+  const startTime = formatDate(dateFrom, Format.TIME);
+  const endTime = formatDate(dateTo, Format.TIME);
+  const duration = formatDuration(getDuration(dateFrom, dateTo));
+  const offersTemplate = createOffersTemplate(selectedOffers);
 
   return `
             <li class="trip-events__item">
               <div class="event">
-                <time class="event__date" datetime="${dateFrom}">${formatDate(dateFrom, Format.DATE)}</time>
+                <time class="event__date" datetime="${dateFrom}">${startDate}</time>
                 <div class="event__type">
                   <img class="event__type-icon" width="42" height="42" src="img/icons/${type}.png" alt="Event type icon">
                 </div>
-                <h3 class="event__title">${capitalizeFirstLetter(type)} ${capitalizeFirstLetter(name)}</h3>
+                <h3 class="event__title">${capitalizedType} ${city}</h3>
                 <div class="event__schedule">
                   <p class="event__time">
-                    <time class="event__start-time" datetime="${dateFrom}">${formatDate(dateFrom, Format.TIME)}</time>
+                    <time class="event__start-time" datetime="${dateFrom}">${startTime}</time>
                     —
-                    <time class="event__end-time" datetime="${dateTo}">${formatDate(dateTo, Format.TIME)}</time>
+                    <time class="event__end-time" datetime="${dateTo}">${endTime}</time>
                   </p>
-                  <p class="event__duration">${formatDuration(getDuration(dateFrom, dateTo))}</p>
+                  <p class="event__duration">${duration}</p>
                 </div>
                 <p class="event__price">
                   €&nbsp;<span class="event__price-value">${basePrice}</span>
@@ -44,7 +50,7 @@ export const createEventsItemTemplate = (event, destination, selectedOffers) => 
                 <h4 class="visually-hidden">Offers:</h4>
                 <ul class="event__selected-offers">
 
-                ${createOffersTemplate(selectedOffers)}
+                ${offersTemplate}
 
                 </ul>
                 <button class="event__favorite-btn
